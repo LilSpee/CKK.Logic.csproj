b33f92a0d5be541f33aa409b3db10447660f1384
4400112554 <4400112554@BT144-PROG23>
2022-04-11 17:45:54 -0600

    Add project files.

---
+++ CKK.Logic/Models/ShoppingCartItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class ShoppingCartItem
+    {
+        private Product _product;
+        private int _quantity;
+
+        public ShoppingCartItem(Product product, int qualtity)
+        {
+            _product = product;
+            _quantity = qualtity;
+        }
+
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        public Product GetProduct()
+        {
+            return _product;
+        }
+
+        public void SetProduct(Product product)
+        {
+            _product = product;
+        }
+
+        public decimal GetTotal()
+        {
+            return _product.GetPrice() * _quantity;          
+        }
+    }
+}
---
+++ CKK.Logic/Models/Store.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class Store
+    {
+        private int _id;
+        private string _name;
+        private Product _product1;
+        private Product _product2;
+        private Product _product3;
+
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public void AddStoreItem(Product prod)
+        {
+            if (_product1 != null)
+            {
+                _product1 = prod;
+            }
+            if (_product2 != null)
+            {
+                _product2 = prod;
+            }
+            if (_product3 != null)
+            {
+                _product3 = prod;
+            }
+            
+        }
+        public void RemoveStoreItem(int productNum)
+        {
+            if(productNum == _product1.GetId())
+            {
+                _product1 = null;
+            }
+        }
+        public Product GetStoreItem(int productNum)
+        {
+            return _product1;
+
+        }
+        public Product FindStoreItemById(int id)
+        {
+            if (id == _product1.GetId())
+            {
+                return _product1;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
